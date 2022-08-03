4227b23313f8864e132474f88b808441df581d8b
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-03 19:42:29 +0900

    Update 1295-find-numbers-with-even-number-of-digits.c

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.c
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.c
@@ -9,3 +9,18 @@ int findNumbers(int* nums, int numsSize) {
 	}
 	return cnt;
 }
+
+int findNumbers(int* nums, int numsSize) {
+	int i, cnt , len = 0;
+	for (int i = 0; i < numsSize; i++) {
+		cnt = 0;
+		while(nums[i]!=0) {
+			nums[i] = nums[i] / 10;
+			++cnt;
+		}
+		if (cnt % 2 == 0) {
+			len++;
+		}
+	}
+	return len;
+}
