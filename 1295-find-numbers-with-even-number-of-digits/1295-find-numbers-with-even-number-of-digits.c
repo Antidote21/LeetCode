2ad7c51f7ff3a098509b5476f885813f7ce8cbb9
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-03 19:41:28 +0900

    Time: 4 ms (87.02%), Space: 6.3 MB (14.53%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.c
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.c
@@ -1,16 +1,11 @@
 
 
-int findNumbers(int* nums, int numsSize){
-    int i, cnt , len = 0;
-	for (int i = 0; i < numsSize; i++) {
-		cnt = 0;
-		while(nums[i]!=0) {
-			nums[i] = nums[i] / 10;
-			++cnt;
-		}
-		if (cnt % 2 == 0) {
-			len++;
+int findNumbers(int* nums, int numsSize) {
+	int i, cnt=0;
+	for (int i = 0; i < numsSize; i++) {		
+		if (((int)floor(log10(nums[i]))-1)%2==0){
+			cnt++;
 		}
 	}
-	return len;
+	return cnt;
 }
