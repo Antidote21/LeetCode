6a8ec6659e83d41d03f97cadef0c63ea249b164b
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-14 20:52:30 +0900

    Time: 129 ms (56.53%), Space: 15.1 MB (77.66%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.c
@@ -0,0 +1,16 @@
+
+
+void moveZeroes(int* nums, int numsSize){
+    int i,j,length=0;
+    for(i=0;i<numsSize;i++){
+        if(nums[i]!=0){
+            nums[length] = nums[i];
+            length++;
+        }      
+    }     
+    for(j=length;j<numsSize;j++){
+         nums[j] = 0;          
+    }
+    return nums;
+
+}
