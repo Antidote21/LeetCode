41b8ab43528f3a9bc0b69f83e5f0328a5026c4b6
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-14 19:40:30 +0900

    Update replace-elements-with-greatest-element-on-right-side.java

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -11,3 +11,25 @@ class Solution {
         return arr;
     }
 }
+
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            if(i==arr.length-1){
+                arr[arr.length-1] = -1;
+                break;
+            }
+            
+            int max=arr[i+1];
+            for (int j = i+1; j < arr.length-1; j++){
+                if(arr[j]<arr[j+1]){
+                    max=Math.max(max,arr[j+1]);
+                }
+            }
+            
+            arr[i]=max;
+        }
+        
+        return arr;
+    }
+}
