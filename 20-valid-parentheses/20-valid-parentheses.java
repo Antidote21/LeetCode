b1aeb1adbd46a1b092a7e8d288507e0a52e2e593
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-09-12 00:21:41 +0900

    Time: 1 ms (99.66%), Space: 40.1 MB (98.02%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        String left = "([{";
+        String right = ")]}";
+        for (char c: s.toCharArray()) {
+            if (left.indexOf(c) > -1) 
+                stack.push(c);
+            else if (stack.empty() || left.indexOf(stack.pop()) != right.indexOf(c))
+                return false;
+        }
+        return stack.empty();
+    }
+}
