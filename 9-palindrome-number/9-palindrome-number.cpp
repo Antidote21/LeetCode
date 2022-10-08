5c2d53d3bf79f9fdade9ffa8675df3b9d1590c97
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-10-09 00:00:28 +0900

    Time: 24 ms (55.55%), Space: 5.9 MB (73.94%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,26 +1,15 @@
-// class Solution {
-// public:
-//     bool isPalindrome(int x) {
-//         string y = to_string(x);
-//         int ylen = y.length();
-//         for(int i=0;i<y.length();i++){
-//             if(y[i]==y[ylen-i-1]&&y[0]!='-'&&y!="10000021"&&y!="1000021"){
-//                 return true;
-//             }else
-//                 return false;
-           
-//         }
-//         return false; 
-//     }
-  
-// };
-
 class Solution {
 public:
     bool isPalindrome(int x) {
         string s = to_string(x);
-        int i = 0, j = s.size()-1;
-        while (i <= j) if (s[i++] != s[j--]) return false;
+        int i = 0;
+        int j = s.length()-1;
+        for(int i=0;i<j;i++){
+            if(s[i]!=s[j--])
+                return false;
+        }
         return true;
     }
+  
 };
+
