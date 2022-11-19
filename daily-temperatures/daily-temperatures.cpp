7b7754fe593e9ea6587bf29c70d55012f74d59a9
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-11-20 03:27:16 +0900

    Time: 472 ms, Memory: 98.6 MB - LeetHub

--- daily-temperatures/daily-temperatures.cpp
+++ daily-temperatures/daily-temperatures.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        
         int n = temperatures.size();
         vector<int>nge(n, 0); 
         stack<int>st;
-        
 		//오르쪽에서 왼쪽으로 이동
         for(int i = n-1; i>=0; --i){
-			
-			// s.top() is the index of elements so we put that index inside temperatures vector to check
-
             while(!st.empty()&&temperatures[st.top()] <= temperatures[i])
                 st.pop();
-				
-            // if stack not empty, then we have some next greater element, 
-			// so we take distance between next greater and current temperature
-			// as we are storing indexes in the stack
             if(!st.empty())
-                nge[i] = st.top()-i; // distance between next greater and current
-            
-			// push the index of current temperature in the stack,
-			// same as pushing current temperature in stack
-           
+                nge[i] = st.top()-i;  
             st.push(i);
         }
-        
         return nge;
     }
 };
