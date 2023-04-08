87bc3657f8df6ab276aef3a9f2cff71fc8027a84
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-04-09 01:54:58 +0900

    Create 46. Permutations.cpp

---
+++ 46 - Permutations/46. Permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>res;//결과값 출력을 위한 2차원 벡터
+        DFS(res, nums, 0);//DFS 실행 
+        return res;//결과값 리턴
+    }
+    void DFS(vector<vector<int>>&res, vector<int>&nums, int pos){
+        if(pos == nums.size() -1 ){ //현재 위치 pos가 배열의 길이와 같을 때 
+            res.push_back(nums);//결과벡터에 nums 벡터를 push_back한다.
+            return;
+        }
+        for(int i = pos; i<nums.size(); i++){
+            swap(nums[pos], nums[i]);//새로운 벡터를 swap함수를 사용해 만들어내고 
+            DFS(res, nums, pos+1);
+            swap(nums[pos], nums[i]);//원상태의 벡터로 바꿈 
+        }
+    }
+};
