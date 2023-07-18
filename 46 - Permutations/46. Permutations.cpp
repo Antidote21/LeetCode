7d80308e9a85c4245566b1477634158c4d09e3ef
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-07-18 23:14:27 +0900

    Update 46. Permutations.cpp

--- 46 - Permutations/46. Permutations.cpp
+++ 46 - Permutations/46. Permutations.cpp
@@ -17,3 +17,30 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void permutations(vector<int>& nums, int ind)
+    {
+        int n=nums.size();
+        if(ind==n)
+            ans.push_back(nums); 
+        
+        for(int i=ind; i<n; i++)
+        {
+            swap(nums[ind], nums[i]);
+            permutations(nums,ind+1);
+            swap(nums[ind], nums[i]);
+        }
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n=nums.size();
+        permutations(nums, 0);
+        
+        return ans;
+    }
+};
